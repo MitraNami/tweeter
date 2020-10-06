71e80e65968662fd09a48156da35e1a4553cad5c
Mitra Nami <a.mxnami@gmail.com>
2020-10-06 19:04:48 +0000

    Insert the number of characters left in the counter with the appropriate color

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,25 +2,24 @@
  * Charecter counter logic
  */
 
+const counterDisplay = (target, num) => {
+  if (num < 0) {
+    target.addClass('red-counter');
+  } else {
+    target.removeClass('red-counter');
+  }
+  target.text(num);
+};
+
+
 $(document).ready(function() {
-  
 
   $('#tweet-text').on('keyup', function() {
     let text = $(this).val();
     let numberOfCharactersLeft = 140 - text.length;
-    console.log(numberOfCharactersLeft);
+    counterjQueryObj = $(this).parent().find('.counter');
+    counterDisplay(counterjQueryObj, numberOfCharactersLeft);
   });
-
-
-
-
-
-
-
-
-
-
-
 
 
 });
