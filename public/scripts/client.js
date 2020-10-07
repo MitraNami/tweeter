5e1d4a8ebbeda2f6ee58f279cd1afe18b5e4b112
Mitra Nami <a.mxnami@gmail.com>
2020-10-07 13:33:21 +0000

    Add all tweet object in tweets array to tweets-container section

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,23 +6,43 @@
 
 $(document).ready(function() {
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+ // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
         "handle": "@SirIsaac"
       },
-    "content": {
+      "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-    "created_at": 1461116232227
- }
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+
+  // Puts all the tweets in tweet container section dynamically
+  const renderTweets = function(tweets) {
+    tweets.forEach((tweet) => {
+      let $tweet = createTweetElement(tweet);
+      $('#tweets-container').append($tweet);
+    });
+  };
 
 
   const createTweetElement  = function(tweet) {
-
     const $tweet = $(`
       <article class="tweet">
       <header>
@@ -47,9 +67,9 @@ $(document).ready(function() {
     return $tweet;
   };
 
-  const $tweet = createTweetElement(tweetData);
 
-  // Test / driver code (temporary)
-  $('#tweets-container').append($tweet);
+  renderTweets(data);
 
 });
+
+
