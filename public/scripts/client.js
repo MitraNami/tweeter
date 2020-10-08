ed32cdeccb4bbfcbfddd116b3b23b4bf49643966
Mitra Nami <a.mxnami@gmail.com>
2020-10-08 21:08:33 +0000

    Refactor the handler of form submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -10,39 +10,7 @@ $(document).ready(function() {
   // Makes an Ajax request (instead of form submit) to send the form data to the server
   // Disallows the submission of an empty or exceeding 140 characters tweet
   const $form = $('section.new-tweet form');
-  $form.on('submit', function(event) {
-    event.preventDefault();
-    // tweet length validation
-    $errorLabel = $(this).parent().find('p');
-    $errorLabel.slideUp();
-    
-    const text = $(this).find('textarea').val();
-    const textLength = text.length;
-    if (!textLength) {
-      $errorLabel.text('Please write something.');
-      $errorLabel.slideDown();
-      return;
-    } else if (textLength > 140) {
-      $errorLabel.text('You have exceeded character limit!');
-      $errorLabel.slideDown();
-      return;
-    }
-
-    const data = $(this).serialize();
-    // Ajax GET request to the server
-    $.ajax({
-      url: '/tweets',
-      method: 'POST',
-      data
-    }).then (() => {
-      //empty out the tweet text if it is sent successfully
-      $(this).find('textarea').val('');
-      $('#tweets-container').empty();
-      loadTweets();
-    });
-
-
-  });
+  $form.on('submit', multiple);
 
 
 
--- public/scripts/helpers.js
+++ public/scripts/helpers.js
@@ -64,7 +64,55 @@ const loadTweets = () => {
 };
 
 
+
+
 //
+const multiple = function(event) {
+  event.preventDefault();
+  // tweet length validation
+  $errorLabel = $(this).parent().find('p');
+  $errorLabel.slideUp();
+  
+  const text = $(this).find('textarea').val();
+  const textLength = text.length;
+  if (!textLength) {
+    $errorLabel.text('Please write something.');
+    $errorLabel.slideDown();
+    return;
+  } else if (textLength > 140) {
+    $errorLabel.text('You have exceeded character limit!');
+    $errorLabel.slideDown();
+    return;
+  }
+
+  const data = $(this).serialize();
+  // Ajax GET request to the server
+  $.ajax({
+    url: '/tweets',
+    method: 'POST',
+    data
+  }).then (() => {
+    //empty out the tweet text if it is sent successfully
+    $(this).find('textarea').val('');
+    $('#tweets-container').empty();
+    loadTweets();
+  });
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
